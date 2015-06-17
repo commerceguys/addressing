7847c57977ed402db3da3a3acfd7657cc29c99e8
dnagirl <dnagirl@users.noreply.github.com>
2015-06-17 23:41:11 +0200

    Fix usage of deprecated method in AddressType.

--- src/Form/Type/AddressType.php
+++ src/Form/Type/AddressType.php
@@ -8,7 +8,7 @@ use CommerceGuys\Addressing\Repository\CountryRepositoryInterface;
 use CommerceGuys\Addressing\Repository\SubdivisionRepositoryInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AddressType extends AbstractType
 {
@@ -56,7 +56,7 @@ class AddressType extends AbstractType
         $builder->addEventSubscriber(new GenerateAddressFieldsSubscriber($this->addressFormatRepository, $this->subdivisionRepository));
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(['data_class' => 'CommerceGuys\Addressing\Model\Address']);
     }
