44e190ff63fe89ca328465cdd4bcf31c39aaab25
Bojan Zivanovic <bojanz@gmail.com>
2020-03-27 18:52:05 +0100

    Reduce the number of available locales. Fixes #140.

--- scripts/generate_country_data.php
+++ scripts/generate_country_data.php
@@ -250,10 +250,27 @@ function discover_locales() {
     // Locales listed without a "-" match all variants.
     // Locales listed with a "-" match only those exact ones.
     $ignoredLocales = [
-        // Interlingua is a made up language.
-        'ia',
+        // Esperanto, Interlingua, Volapuk are made up languages.
+        'eo', 'ia', 'vo',
+        // Church Slavic, Manx, Prussian are historical languages.
+        'cu', 'gv', 'prg',
         // Valencian differs from its parent only by a single character (è/é).
         'ca-ES-VALENCIA',
+        // Africa secondary languages.
+        'agq', 'ak', 'am', 'asa', 'bas', 'bem', 'bez', 'bm', 'cgg', 'dav',
+        'dje', 'dua', 'dyo', 'ebu', 'ee', 'ewo', 'ff', 'ff-Latn', 'guz',
+        'ha', 'ig', 'jgo', 'jmc', 'kab', 'kam', 'kea', 'kde', 'ki', 'kkj',
+        'kln', 'khq', 'ksb', 'ksf', 'lag', 'luo', 'luy', 'lu', 'lg', 'ln',
+        'mas', 'mer', 'mua', 'mgo', 'mgh', 'mfe', 'naq', 'nd', 'nmg', 'nnh',
+        'nus', 'nyn', 'om', 'rof', 'rwk', 'saq', 'seh', 'ses', 'sbp', 'sg',
+        'shi', 'sn', 'teo', 'ti', 'tzm', 'twq', 'vai', 'vai-Latn', 'vun',
+        'wo', 'xog', 'xh', 'zgh', 'yav', 'yo', 'zu',
+        // Europe secondary languages.
+        'br', 'dsb', 'fo', 'fur', 'fy', 'hsb', 'ksh', 'kw', 'nds', 'or', 'rm',
+        'se', 'smn', 'wae',
+        // Other infrequently used locales.
+        'ceb', 'ccp', 'chr', 'ckb', 'haw', 'ii', 'jv', 'kl', 'kn', 'lkt',
+        'lrc', 'mi', 'mzn', 'os', 'qu', 'row', 'sah', 'tt', 'ug', 'yi',
         // Special "grouping" locales.
         'root', 'en-US-POSIX',
     ];
