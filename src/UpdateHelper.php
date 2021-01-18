1bf83b7a8bc78ce02df7d71757efb4fac547433e
Matthijs Dabroek <dabroek@users.noreply.github.com>
2021-01-18 21:56:32 +0100

    Ensure PHP 7.4 support and drop support for PHP 7.0 (EOL) (#156)

--- src/UpdateHelper.php
+++ src/UpdateHelper.php
@@ -16,7 +16,7 @@ class UpdateHelper
      *
      * @var array
      */
-    static protected $subdivisionUpdateMap = [];
+    protected static $subdivisionUpdateMap = [];
 
     /**
      * Splits the recipient into givenName and familyName fields.
