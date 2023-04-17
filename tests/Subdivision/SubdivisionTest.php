e40dfe15d7b6fbfba5cd7695ce9a586baf63ae2e
Bojan Zivanovic <bojanz@gmail.com>
2023-04-17 18:36:39 +0200

    Remove a @covers annotation that points to a deleted method.

--- tests/Subdivision/SubdivisionTest.php
+++ tests/Subdivision/SubdivisionTest.php
@@ -34,7 +34,6 @@ final class SubdivisionTest extends TestCase
      * @covers ::getLocalName
      * @covers ::getIsoCode
      * @covers ::getPostalCodePattern
-     * @covers ::getPostalCodePatternType
      * @covers ::getChildren
      * @covers ::hasChildren
      */
